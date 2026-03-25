1e7de88b8c7763c268ba47d2e4c3911d13afd9d2
hungg261 <phanthanhhung.26012010@gmail.com>
2026-03-25 14:55:14 +0700

    Update codeforces/2000F.cpp

--- codeforces/2000F.cpp
+++ codeforces/2000F.cpp
@@ -1,30 +1,44 @@
 /******************************************************************************
 Link: https://codeforces.com/contest/2000/problem/F
 Code: 2000F
-Time (YYYY-MM-DD-hh.mm.ss): 2026-03-23-20.34.19
+Time (YYYY-MM-DD-hh.mm.ss): 2026-03-24-10.59.13
 *******************************************************************************/
 #include<bits/stdc++.h>
 using namespace std;
 
 struct Rect{
-    int n, m, idx;
+    int n, m;
 
-    void input(int i){
+    void input(){
         cin >> n >> m;
-        idx = i;
-
         if(n > m) swap(n, m);
     }
 
-    bool operator < (const Rect& other) const{
-        return n > other.n || (n == other.n && idx < other.idx);
+    pair<int, int> use(){
+        if(n == 1 && m == 1){
+            --n; --m;
+            return {2, 1};
+        }
+
+
+        pair<int, int> res = {1, n};
+
+        --m;
+        if(n > m) swap(n, m);
+
+        return res;
     }
 
-    int C() const{ return n + m; }
-    int S() const{ return n * m; }
+    bool valid(){
+        return n > 0 && m > 0;
+    }
+
+    bool operator < (const Rect& other) const{
+        return n > other.n;
+    }
 
     void output() const{
-        cerr << idx << ": " << n << " " << m << "\n";
+        cerr << n << " " << m << "\n";
     }
 };
 
@@ -34,7 +48,7 @@ void solve(){
 
     vector<Rect> rects(n + 1);
     for(int i = 1; i <= n; ++i){
-        rects[i].input(i);
+        rects[i].input();
     }
 
     priority_queue<Rect> pq;
@@ -43,26 +57,32 @@ void solve(){
 
     int res = 0;
     while(k > 0){
-        Rect cur = pq.top();
+        if(pq.empty()) break;
+
+        Rect rect = pq.top();
         pq.pop();
 
-        if(cur.C() <= k){
-            k -= cur.C();
-            res += cur.S();
-            continue;
-        }
+        int C, S;
+        tie(C, S) = rect.use();
 
+        cerr << C << " " << S << " ";
+        rect.output();
 
+        k -= C;
+        res += S;
+
+        if(rect.valid()) pq.push(rect);
     }
 
-
+    if(k > 0) cout << "-1\n";
+    else cout << res << "\n";
 }
 
 signed main(){
     ios_base::sync_with_stdio(0); cin.tie(0);
 
     int t = 1;
-    //cin >> t;
+//    cin >> t;
 
     while(t--){
         solve();
