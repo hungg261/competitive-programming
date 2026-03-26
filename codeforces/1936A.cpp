2341290a2c4789d77f87d122cf111ec7f4b60c78
hungg261 <phanthanhhung.26012010@gmail.com>
2026-03-26 23:11:20 +0700

    Auto-commit [2026-03-26 23:11:20]

---
+++ codeforces/1936A.cpp
@@ -0,0 +1,24 @@
+/******************************************************************************
+Link: https://codeforces.com/problemset/problem/1936/A
+Code: 1936A
+Time (YYYY-MM-DD-hh.mm.ss): 2026-03-26-23.09.06
+*******************************************************************************/
+#include<bits/stdc++.h>
+using namespace std;
+
+void solve(){
+
+}
+
+signed main(){
+    ios_base::sync_with_stdio(0); cin.tie(0);
+
+    int t = 1;
+    //cin >> t;
+
+    while(t--){
+        solve();
+    }
+
+    return 0;
+}
