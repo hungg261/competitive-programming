9582e1fff097132d693103e3b973219a5870e9c7
hungg261 <phanthanhhung.26012010@gmail.com>
2026-04-01 23:39:58 +0700

    Auto-commit [2026-04-01 23:39:57]

---
+++ codeforces/2214G.cpp
@@ -0,0 +1,27 @@
+/******************************************************************************
+Link: https://codeforces.com/contest/2214/problem/G
+Code: 2214G
+Time (YYYY-MM-DD-hh.mm.ss): 2026-04-01-23.17.11
+*******************************************************************************/
+#include<bits/stdc++.h>
+using namespace std;
+
+void solve(){
+    int i;
+    cin >> i;
+
+    cout << "bigchadjeff"[i - 1] << "\n";
+}
+
+signed main(){
+    ios_base::sync_with_stdio(0); cin.tie(0);
+
+    int t = 1;
+    cin >> t;
+
+    while(t--){
+        solve();
+    }
+
+    return 0;
+}
