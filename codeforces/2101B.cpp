64a6b0b31029855a1ae4f77f3a5a80ccb0e0829a
hungg261 <phanthanhhung.26012010@gmail.com>
2026-04-06 19:25:08 +0700

    Auto-commit [2026-04-06 19:25:08]

--- codeforces/2101B.cpp
+++ codeforces/2101B.cpp
@@ -6,41 +6,76 @@ Time (YYYY-MM-DD-hh.mm.ss): 2026-04-06-17.18.15
 #include<bits/stdc++.h>
 using namespace std;
 
+struct FenwickTree{
+    int n;
+    vector<int> BIT;
+
+    FenwickTree() = default;
+    FenwickTree(int sz){ n = sz; BIT.resize(sz + 1, 0); }
+
+    void update(int idx, int val){
+        if(idx <= 0) return;
+
+        for(int i = idx; i <= n; i += i & -i){
+            BIT[i] += val;
+        }
+    }
+
+    int get(int idx){
+        if(idx <= 0) return 0;
+
+        int res = 0;
+        for(int i = idx; i > 0; i -= i & -i){
+            res += BIT[i];
+        }
+        return res;
+    }
+
+    int get(int l, int r){ return get(r) - get(l - 1); }
+};
+
+int inv(const deque<int>& arr, const int& MAX){
+    FenwickTree fwt(MAX);
+    int res = 0;
+    for(int i = (int)arr.size() - 1; i >= 0; --i){
+        res += fwt.get(arr[i]);
+        fwt.update(arr[i], 1);
+    }
+    return res;
+}
+
 void solve(){
     int n;
     cin >> n;
 
-    vector<int> a(n);
-    for(int i = 0; i < n; ++i)
-        cin >> a[i];
-
-    vector<vector<int>> dp(n + 1);
-    dp[n] = {};
-    for(int i = n - 1; i > n - 4 && i >= 0; --i){
-        dp[i].push_back(a[i]);
-        for(int x: dp[i + 1]) dp[i].push_back(x);
+    array<deque<int>, 2> arr;
+    for(int i = 0; i < n; ++i){
+        int cur; cin >> cur;
+        arr[i & 1].push_back(cur);
     }
 
-    for(int i = n - 4; i >= 0; --i){
-        if(a[i + 2] < a[i] || (a[i + 2] == a[i] && a[i + 3] < a[i + 1])){
-            dp[i] = {a[i + 2], a[i + 3], a[i], a[i + 1]};
-            for(int x: dp[i + 4]) dp[i].push_back(x);
-        }
-        else{
-            dp[i].push_back(a[i]);
-            for(int x: dp[i + 1]) dp[i].push_back(x);
-        }
+    bool c = (inv(arr[0], n) & 1) ^ (inv(arr[1], n) & 1);
+    sort(begin(arr[0]), end(arr[0]));
+    sort(begin(arr[1]), end(arr[1]));
+
+    vector<int> res(n);
+
+    for(int i = 0; i < n; ++i){
+        res[i] = arr[i & 1].front();
+        arr[i & 1].pop_front();
     }
 
-    for(int x: dp[0]) cout << x << " ";
-    cout << "\n";
+    bool new_c = (inv(arr[0], n) & 1) ^ (inv(arr[1], n) & 1);
+    if(c != new_c) swap(res[n - 3], res[n - 1]);
+
+    for(int ele: res) cout << ele << " "; cout << "\n";
 }
 
 signed main(){
     ios_base::sync_with_stdio(0); cin.tie(0);
 
     int t = 1;
-    //cin >> t;
+    cin >> t;
 
     while(t--){
         solve();
