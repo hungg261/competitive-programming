3c6f624e14ff0787ee44c5bccb663484880b3c93
hungg261 <phanthanhhung.26012010@gmail.com>
2026-04-13 21:45:34 +0700

    Auto-commit [2026-04-13 21:45:34]

--- cses/3228.cpp
+++ cses/3228.cpp
@@ -6,7 +6,7 @@ Time (YYYY-MM-DD-hh.mm.ss): 2026-04-13-17.48.26
 #include<bits/stdc++.h>
 using namespace std;
 
-const int MAXN = 1000, MAXLG = 10;
+const int MAXN = 1000;
 int n;
 struct State{
     int l, r;
@@ -19,7 +19,7 @@ struct State{
     }
 };
 
-vector<State> states[MAXLG + 1];
+vector<State> states[2];
 
 void solve(){
     vector<int> res(n + 1, -1);
@@ -27,39 +27,43 @@ void solve(){
     states[0].emplace_back();
     states[0].back().init();
 
-    for(int h = 0; h < MAXLG; ++h){
-        if(states[h].empty()) break;
+    for(int h = 0; ; ++h){
+        if(states[h & 1].empty()) break;
 
         vector<int> b(n + 1);
-        for(const State& cur: states[h]){
-            int len = cur.pos.size();
-            int mid = (len - 1) >> 1;
-
-            for(int i = 0; i < len; ++i){
-                b[cur.pos[i]] = i <= mid;
-            }
-        }
-
-        cout << "? ";
-        for(int i = 1; i <= n; ++i)
-            cout << b[i];
-        cout << endl;
-
-
-        string jury;
-        cin >> jury;
-
-        jury = "#" + jury;
-        while(!states[h].empty()){
-            const State cur = states[h].back();
-            states[h].pop_back();
-
+        bool enough = true;
+        for(const State& cur: states[h & 1]){
             if(cur.l == cur.r){
                 assert(cur.pos.size() == 1);
 
                 res[cur.pos[0]] = cur.l;
                 continue;
             }
+
+            int mid = (cur.l + cur.r) >> 1;
+            for(int i = cur.l; i <= cur.r; ++i){
+                b[i] = i <= mid;
+            }
+
+            enough = false;
+        }
+
+        if(enough) break;
+
+        cout << "? ";
+        for(int i = 1; i <= n; ++i)
+            cout << b[i];
+        cout << endl;
+
+        string jury;
+        cin >> jury;
+
+        jury = "#" + jury;
+        while(!states[h & 1].empty()){
+            const State cur = states[h & 1].back();
+            states[h & 1].pop_back();
+
+            if(cur.l == cur.r) continue;
 
             vector<int> posL, posR;
             for(int i: cur.pos){
@@ -69,8 +73,8 @@ void solve(){
             }
 
             int mid = (cur.l + cur.r) >> 1;
-            states[h + 1].push_back({cur.l, mid, posL});
-            states[h + 1].push_back({mid + 1, cur.r, posR});
+            states[h + 1 & 1].push_back({cur.l, mid, posL});
+            states[h + 1 & 1].push_back({mid + 1, cur.r, posR});
         }
     }
 
