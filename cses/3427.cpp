439bfd24e90a75596ca96321d4a8a74b3f4d6c53
hungg261 <phanthanhhung.26012010@gmail.com>
2026-04-16 18:29:56 +0700

    Auto-commit [2026-04-16 18:29:56]

--- cses/3427.cpp
+++ cses/3427.cpp
@@ -8,94 +8,25 @@ using namespace std;
 
 #define int long long
 
-struct Fraction{
-    int a, b;
-
-    Fraction() = default;
-    Fraction(int _a, int _b = 1): a(_a), b(_b){ simplify(); }
-
-    friend istream& operator >> (istream& is, Fraction& frac){
-        return is >> frac.a >> frac.b;
-    }
-
-    friend ostream& operator << (ostream& os, const Fraction& frac){
-        return os << frac.a << "/" << frac.b;
-    }
-
-    Fraction inverse() const { return Fraction(b, a); }
-
-    Fraction simplified() const {
-        int g = __gcd(a, b);
-        return Fraction(a / g, b / g);
-    }
-
-    void simplify(){
-        int g = __gcd(a, b);
-        a /= g;
-        b /= g;
-    }
-
-    int normalize(){
-        assert(a % b == 0);
-        return a / b;
-    }
-
-    bool operator == (const Fraction& other) const { return a * other.b == other.a * b; }
-    bool operator != (const Fraction& other) const { return a * other.b != other.a * b; }
-    bool operator < (const Fraction& other) const { return a * other.b < other.a * b; }
-    bool operator > (const Fraction& other) const { return a * other.b > other.a * b; }
-    bool operator <= (const Fraction& other) const { return *this < other || *this == other; }
-    bool operator >= (const Fraction& other) const { return *this < other || *this == other; }
-    Fraction operator - () const { return Fraction(-a, b); }
-
-    Fraction operator + (const Fraction& other) const {
-        int l = b / __gcd(b, other.b) * other.b;
-
-        int a1 = a * l / b, a2 = other.a * l / other.b;
-        return Fraction(a1 + a2, l).simplified();
-    }
-
-    Fraction operator - (const Fraction& other) const {
-        return *this + Fraction(-other.a, other.b);
-    }
-
-    Fraction operator * (const Fraction& other) const {
-        return Fraction(a * other.a, b * other.b).simplified();
-    }
-
-    Fraction operator / (const Fraction& other) const {
-        return *this * other.inverse();
-    }
-
-    Fraction operator + (int other) const { return *this + Fraction(other); }
-    Fraction operator - (int other) const { return *this - Fraction(other); }
-    Fraction operator * (int other) const { return *this * Fraction(other); }
-    Fraction operator / (int other) const { return *this / Fraction(other); }
-};
-
 struct Line{
-    Fraction m, c;
+    int m, c;
     int idx;
 
     Line() = default;
-    Line(int _m, int _c){
-        m = Fraction(_m, 1);
-        c = Fraction(_c, 1);
-    }
+    Line(int _m, int _c): m(_m), c(_c){}
     Line(int x1, int y1, int x2, int y2){
-        m = Fraction(y2 - y1, x2 - x1);
+        m = (y2 - y1) / (x2 - x1);
         c = - m * x1 + y1;
     }
 
-    Fraction f(int x) const { return m * x + c; }
-    Fraction f(Fraction x) const { return m * x + c; }
-    Fraction slope() const { return m; }
+    int f(int x) const { return m * x + c; }
+    int slope() const { return m; }
 
-    pair<Fraction, Fraction> intersect(const Line& other) const {
+    pair<int, int> intersect(const Line& other) const {
         assert(m != other.m);
 
-        Fraction x = Fraction(c - other.c) / Fraction(other.m - m);
-        Fraction y = f(x);
+        int x = (c - other.c) / (other.m - m);
+        int y = f(x);
 
         return {x, y};
     }
@@ -128,12 +59,13 @@ signed main(){
     vector<int> sta;
     int sz = 0;
     for(int i = 0; i < n; ++i){
+        if(sz > 0 && l[sta[sz - 1]].m == l[i].m) sta.pop_back(), --sz;
         while(sz >= 2){
             const Line& last1 = l[sta[sz - 1]];
             const Line& last2 = l[sta[sz - 2]];
             const Line& cur = l[i];
 
-            if(last2.intersect(last1).second < last1.intersect(cur).second){
+            if(last2.intersect(last1) >= last1.intersect(cur)){
                 sta.pop_back();
                 --sz;
             }
@@ -146,10 +78,10 @@ signed main(){
 
     int i = 0;
     for(int x = 0; x <= m; ++x){
-        while(i + 1 < n && l[i].f(x) <= l[i + 1].f(x))
+        while(i + 1 < sz && l[sta[i]].f(x) <= l[sta[i + 1]].f(x))
             ++i;
 
-        cout << l[i].f(x).normalize() << " ";
+        cout << l[sta[i]].f(x) << " ";
     }
 
     return 0;
