aa184afdd9d72e6f41bd7bd6c06f351a535e245d
hungg261 <phanthanhhung.26012010@gmail.com>
2026-04-21 09:15:54 +0700

    Auto-commit [2026-04-21 09:15:54]

---
+++ cses/2074.cpp
@@ -0,0 +1,28 @@
+/******************************************************************************
+Link: https://cses.fi/problemset/task/2074
+Code: 2074
+Time (YYYY-MM-DD-hh.mm.ss): 2026-04-21-09.03.20
+*******************************************************************************/
+#include<bits/stdc++.h>
+using namespace std;
+
+namespace iTreap{
+
+struct Node{
+    int sum;
+    int priority, sz;
+    bool rev;
+    Node *l, *r;
+
+    Node()
+};
+
+}
+using namespace iTreap;
+
+signed main(){
+    ios_base::sync_with_stdio(0); cin.tie(0);
+
+
+    return 0;
+}
