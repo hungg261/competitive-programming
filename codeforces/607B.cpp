6a2fd38f6392bd1a000843e787ec17701344152c
hungg261 <phanthanhhung.26012010@gmail.com>
2026-04-26 22:48:40 +0700

    Auto-commit [2026-04-26 22:48:39]

--- codeforces/607B.cpp
+++ codeforces/607B.cpp
@@ -6,42 +6,42 @@ Time (YYYY-MM-DD-hh.mm.ss): 2026-04-26-10.02.13
 #include<bits/stdc++.h>
 using namespace std;
 
+const int MAXN = 500;
+int n, c[MAXN + 5];
+int dp[MAXN + 5][MAXN + 5];
+
+void solve(){
+    for(int i = 1; i <= n; ++i){
+        dp[i][i] = 1;
+        dp[i][i + 1] = (c[i] == c[i + 1] ? 1 : 2);
+    }
+
+    for(int len = 3; len <= n; ++len){
+        for(int l = 1; l + len - 1 <= n; ++l){
+            int r = l + len - 1;
+
+            dp[l][r] = dp[l + 1][r] + 1;
+            for(int k = l + 1; k <= r; ++k){
+                dp[l][r] = min(dp[l][r], dp[l][k] + dp[k + 1][r]);
+
+                if(k > l + 1 && c[l] == c[k])
+                    dp[l][r] = min(dp[l][r], dp[l + 1][k - 1] + dp[k + 1][r]);
+            }
+        }
+    }
+
+    cout << dp[1][n] << "\n";
+}
+
 signed main(){
     ios_base::sync_with_stdio(0); cin.tie(0);
 
-    int n;
     cin >> n;
-
-    vector<int> a(n);
-    for(int i = 0; i < n; ++i)
-        cin >> a[i];
-
-    int res = 0;
-    while(!a.empty()){
-        n = a.size();
-        pair<int, int> LPS = {-1, -1};
-        for(int i = 0; i < n; ++i){
-            if(i + 1 < n && a[i] == a[i + 1]){
-                int L = i, R = i + 1;
-                while(a[L] == a[R])
-                    --L, ++R;
-
-                ++L; --R;
-                LPS = max(LPS, {R - L + 1, L});
-            }
-
-            int L = i, R = i;
-            while(a[L] == a[R]) --L, ++R;
-
-            ++L; --R;
-            LPS = max(LPS, {R - L + 1, L});
-        }
-
-        a.erase(begin(a) + LPS.second, begin(a) + LPS.second + LPS.first);
-        ++res;
+    for(int i = 1; i <= n; ++i){
+        cin >> c[i];
     }
 
-    cout << res << "\n";
+    solve();
 
     return 0;
 }
