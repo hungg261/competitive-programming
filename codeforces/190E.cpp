a897a2b826afa6945810b895d1212ae6a5d6093b
hungg261 <phanthanhhung.26012010@gmail.com>
2026-05-03 12:07:37 +0700

    Auto-commit [2026-05-03 12:07:37]

---
+++ codeforces/190E.cpp
@@ -0,0 +1,14 @@
+/******************************************************************************
+Link: https://codeforces.com/problemset/problem/190/E
+Code: 190E
+Time (YYYY-MM-DD-hh.mm.ss): 2026-05-03-11.53.11
+*******************************************************************************/
+#include<bits/stdc++.h>
+using namespace std;
+
+signed main(){
+    ios_base::sync_with_stdio(0); cin.tie(0);
+    
+
+    return 0;
+}
