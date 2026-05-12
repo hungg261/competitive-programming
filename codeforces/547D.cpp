37043311de2ad616d11111f9a3a67fa119a56d0a
hungg261 <phanthanhhung.26012010@gmail.com>
2026-05-12 14:46:02 +0700

    Auto-commit [2026-05-12 14:46:02]

--- codeforces/547D.cpp
+++ codeforces/547D.cpp
@@ -7,155 +7,86 @@ Time (YYYY-MM-DD-hh.mm.ss): 2026-05-11-19.48.52
 using namespace std;
 
 const int INF = 1e9;
+const int MAXN = 2e5, MAXVAL = 2e5;
+set<pair<int, int>> adj[2][MAXN + 5];
+set<pair<int, int>> odds;
 
-struct MaxFlow{
-    struct EdgeTo{
-        int v, w, rev;
-    };
-    struct Edge{
-        int u, v, w;
-    };
+int n;
+vector<pair<int, int>> P;
+int delta[2][MAXN + 5];
+char colors[MAXN + 5];
 
-    int n;
-    vector<vector<EdgeTo>> adj;
-    vector<Edge> edges;
-    vector<int> level;
-    vector<int> it;
+void hierholzer(int u, int b){
+    while(!adj[b][u].empty()){
+        int v, id; tie(v, id) = *adj[b][u].begin();
+        adj[b][u].erase(adj[b][u].begin());
+        adj[b ^ 1][v].erase({u, id});
 
-    MaxFlow() = default;
-    MaxFlow(int sz): n(sz){
-        adj.resize(sz + 1);
-        level.resize(sz + 1);
-        it.resize(sz + 1, 0);
+        colors[id] = b ? 'r' : 'b';
+        delta[b][u] += b ? 1 : -1;
+        delta[b ^ 1][v] += b ? 1 : -1;
+
+        hierholzer(v, b ^ 1);
+    }
+}
+
+void dfs(){
+    if(odds.empty()){
+        for(int u = 1; u <= MAXN; ++u)
+            hierholzer(u, 0);
+        return;
     }
 
-    void addEdge(int u, int v, int w, bool directed = true){
-        edges.push_back({u, v, w});
-        adj[u].push_back({v, w, (int)adj[v].size()});
-        adj[v].push_back({u, directed ? 0 : w, (int)adj[u].size() - 1});
+    int u, b; tie(u, b) = *odds.begin();
+    int v, id; tie(v, id) = *adj[b][u].begin();
+
+    {
+        adj[b][u].erase(adj[b][u].begin());
+        adj[b ^ 1][v].erase({u, id});
+
+        odds.erase(odds.begin());
+        if(adj[b ^ 1][v].size() & 1) odds.insert({v, b ^ 1});
+        else odds.erase({v, b ^ 1});
     }
 
-    bool bfs_push(int s, int t){
-        fill(begin(level) + 1, end(level), -1);
-        queue<int> que;
-
-        que.push(s);
-        level[s] = 0;
-        while(!que.empty()){
-            int u = que.front(); que.pop();
-            for(const EdgeTo& e: adj[u]){
-                if(e.w > 0 && level[e.v] == -1){
-                    level[e.v] = level[u] + 1;
-                    que.push(e.v);
-                }
-            }
-        }
-
-        return level[t] >= 0;
+    dfs();
+    if(delta[b ^ 1][v] < 0){
+        colors[id] = 'r';
+        delta[b][u]++;
+        delta[b ^ 1][v]++;
     }
-
-    int dfs_sendFlow(int u, int t, int flow){
-        if(flow == 0 || u == t){
-            return flow;
-        }
-
-        for(int& i = it[u]; i < (int)adj[u].size(); ++i){
-            EdgeTo& e = adj[u][i];
-            if(e.w > 0 && level[u] + 1 == level[e.v]){
-                int pushed = dfs_sendFlow(e.v, t, min(flow, e.w));
-
-                if(pushed > 0){
-                    e.w -= pushed;
-                    adj[e.v][e.rev].w += pushed;
-                    return pushed;
-                }
-            }
-        }
-
-        return 0;
+    else{
+        colors[id] = 'b';
+        delta[b][u]--;
+        delta[b ^ 1][v]--;
     }
+}
 
-    void dfs_findCut(int u, vector<bool>& mark){
-        if(mark[u]) return;
-        mark[u] = true;
-
-        for(const EdgeTo& e: adj[u]){
-            if(e.w > 0) dfs_findCut(e.v, mark);
-        }
-    }
-
-    vector<Edge> getMinCut(int s){
-        vector<bool> mark(n + 1, false);
-        dfs_findCut(s, mark);
-
-        vector<Edge> cuts;
-        for(const Edge& e: edges){
-            if(mark[e.u] ^ mark[e.v]) cuts.emplace_back(e);
-        }
-
-        return cuts;
-    }
-
-    int getFlow(int s, int t){
-        int res = 0;
-        while(bfs_push(s, t)){
-            fill(begin(it) + 1, end(it), 0);
-            while(int pushed = dfs_sendFlow(s, t, INF))
-                res += pushed;
-        }
-
-        return res;
-    }
-};
-
-const int MAXN = 2e5;
-int deg[MAXN * 2 + 5];
 void solve(){
-    int n;
     cin >> n;
-
-    MaxFlow flow(MAXN * 2 + 2);
-    const int S = MAXN * 2 + 1, T = MAXN * 2 + 2;
-
-    vector<pair<int, int>> P;
-    for(int i = 1; i <= n; ++i){
+    for(int i = 0; i < n; ++i){
         int x, y;
         cin >> x >> y;
 
-        flow.addEdge(x, MAXN + y, 1);
-        ++deg[x];
-        ++deg[MAXN + y];
+        adj[0][x].insert({y, i});
+        adj[1][y].insert({x, i});
 
         P.emplace_back(x, y);
     }
 
-    for(int i = 1; i <= MAXN; ++i){
-        int w = (deg[i] + 1) / 2;
-        if(w) flow.addEdge(S, i, w);
-    }
-
-    for(int j = 1; j <= MAXN; ++j){
-        int w = (deg[MAXN + j] + 1) / 2;
-        if(w) flow.addEdge(MAXN + j, T, w);
-    }
-
-    flow.getFlow(S, T);
-
-    vector<pair<int, int>> chosen;
-    for(int x = 1; x <= MAXN; ++x){
-        for(const MaxFlow::EdgeTo& e: flow.adj[x]){
-            if(MAXN < e.v && e.v <= MAXN * 2 && e.w == 0){
-                chosen.emplace_back(x, e.v - MAXN);
+    for(int b = 0; b < 2; ++b){
+        for(int u = 1; u <= MAXN; ++u){
+            if(adj[b][u].size() & 1){
+                odds.insert({u, b});
             }
         }
     }
 
-    sort(begin(chosen), end(chosen));
-    for(const pair<int, int>& p: P){
-        if(binary_search(begin(chosen), end(chosen), p)) cout << "b";
-        else cout << "r";
+    dfs();
+
+    for(int i = 0; i < n; ++i){
+        cout << colors[i];
     }
-    cout << "\n";
 }
 
 signed main(){
