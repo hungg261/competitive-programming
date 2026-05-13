36f5103fc804a99034ead38fe93d5abcf8dc7846
hungg261 <phanthanhhung.26012010@gmail.com>
2026-05-13 09:52:09 +0700

    Auto-commit [2026-05-13 09:52:09]

---
+++ codeforces/Group_7Dn3ObOpau_513385A.cpp
@@ -0,0 +1,35 @@
+/******************************************************************************
+Link: https://codeforces.com/group/7Dn3ObOpau/contest/513385/problem/A
+Code: Group_7Dn3ObOpau_513385A
+Time (YYYY-MM-DD-hh.mm.ss): 2026-05-12-20.52.29
+*******************************************************************************/
+#include<bits/stdc++.h>
+using namespace std;
+
+void solve(){
+    int n;
+    cin >> n;
+
+    vector<int> a(n + 1);
+    long long sum = 0;
+    for(int i = 1; i <= n; ++i){
+        cin >> a[i];
+        sum
+    }
+    sort(begin(a) + 1, end(a));
+
+
+}
+
+signed main(){
+    ios_base::sync_with_stdio(0); cin.tie(0);
+
+    int t = 1;
+    cin >> t;
+
+    while(t--){
+        solve();
+    }
+
+    return 0;
+}
