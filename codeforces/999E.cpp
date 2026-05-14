15bfb461c7e826c5040d42996f3cd81472915bdb
hungg261 <phanthanhhung.26012010@gmail.com>
2026-05-14 17:44:53 +0700

    Auto-commit [2026-05-14 17:44:53]

--- codeforces/999E.cpp
+++ codeforces/999E.cpp
@@ -7,119 +7,89 @@ Time (YYYY-MM-DD-hh.mm.ss): 2026-05-13-14.18.24
 using namespace std;
 
 const int MAXN = 5000;
-const int INF = INT_MAX / 2;
-int n, m, s, adj[MAXN + 5][MAXN + 5];
-bool chosen[MAXN + 5][MAXN + 5];
+int n, m, s;
+vector<int> adj[MAXN + 5], rev_adj[MAXN + 5];
 
-vector<int> G[MAXN + 5];
-int e[MAXN + 5], par[MAXN + 5];
-int mark[MAXN + 5];
-
+vector<int> topo;
+bool mark[MAXN + 5];
 void dfs(int u){
-    mark[u] = 1;
+    if(mark[u]) return;
+    mark[u] = true;
 
-    for(int v: G[u]){
-        if(mark[v] == 1){
-            e[u] = INF;
-            e[v] = INF;
-            continue;
-        }
-        else if(mark[v] == 0){
-            if(e[u] == INF) e[v] = INF;
-            else if(e[u] + 1 > e[v]){
-                e[v] = e[u] + 1;
-                par[v] = u;
-            }
-            dfs(v);
-        }
-
-        if(e[u] == INF || e[v] == INF){
-            e[u] = INF;
-            e[v] = INF;
-        }
-        else if(e[u] + 1 > e[v]){
-            e[v] = e[u] + 1;
-            par[v] = u;
-        }
+    for(int v: adj[u]){
+        dfs(v);
     }
 
-    mark[u] = 2;
+    topo.push_back(u);
 }
 
-void dijkstra(){
-    vector<int> d(n + 1, INF);
-    vector<bool> vis(n + 1, false);
+int scc_id[MAXN + 5], scc_cnt = 0;
+void kosaraju(int u){
+    if(scc_id[u]) return;
+    scc_id[u] = scc_cnt;
 
-    d[s] = 0;
-    for(int i = 1; i <= n; ++i){
-        int u = -1;
-        for(int j = 1; j <= n; ++j){
-            if(!vis[j] && (u == -1 || d[j] < d[u])){
-                u = j;
-            }
-        }
-
-        if(d[u] == INF) break;
-        vis[u] = true;
-
-        for(int v = 1; v <= n; ++v){
-            if(vis[v]) continue;
-
-            int newCost = d[u] + adj[u][v];
-            if(newCost < d[v]){
-                d[v] = newCost;
-            }
-        }
+    for(int v: rev_adj[u]){
+        kosaraju(v);
     }
+}
 
+vector<int> dag[MAXN + 5];
+int indeg[MAXN + 5];
+void compress_dag(){
     for(int u = 1; u <= n; ++u){
-        for(int v = 1; v <= n; ++v){
-            if(d[u] + adj[u][v] == d[v]){
-                G[u].push_back(v);
+        for(int v: adj[u]){
+            if(scc_id[u] != scc_id[v]){
+                dag[scc_id[u]].push_back(scc_id[v]);
             }
         }
     }
 
-    memset(e, -1, sizeof e);
-    memset(par, -1, sizeof par);
-    e[s] = 0;
-    dfs(s);
-
-    int res = 0;
-    for(int v = 1; v <= n; ++v){
-        if(e[v] == INF || e[v] == -1) continue;
-
-        int cur = v;
-        while(par[cur] != -1){
-            int prv = par[cur];
-
-            if(adj[prv][cur] == 1 && !chosen[prv][cur]){
-                chosen[prv][cur] = true;
-                ++res;
-            }
-            cur = prv;
-        }
+    for(int i = 1; i <= scc_cnt; ++i){
+        sort(dag[i].begin(), dag[i].end());
+        dag[i].erase(unique(dag[i].begin(), dag[i].end()), dag[i].end());
     }
 
-    cout << res << "\n";
+    for(int u = 1; u <= scc_cnt; ++u){
+        for(int v: dag[u]){
+            ++indeg[v];
+        }
+    }
 }
 
 signed main(){
     ios_base::sync_with_stdio(0); cin.tie(0);
-
     cin >> n >> m >> s;
-    for(int u = 1; u <= n; ++u)
-        for(int v = 1; v <= n; ++v)
-            adj[u][v] = 1;
-
     for(int i = 1; i <= m; ++i){
         int a, b;
         cin >> a >> b;
 
-        adj[a][b] = 0;
+        adj[a].push_back(b);
+        rev_adj[b].push_back(a);
     }
 
-    dijkstra();
+    for(int u = 1; u <= n; ++u){
+        if(!mark[u]) dfs(u);
+    }
+    reverse(begin(topo), end(topo));
+
+    for(int u: topo){
+        if(scc_id[u] == 0){
+            ++scc_cnt;
+            kosaraju(u);
+        }
+    }
+
+    compress_dag();
+
+    int res = 0;
+    for(int i = 1; i <= scc_cnt; ++i){
+        if(indeg[i] == 0 && i != scc_id[s]){
+            ++res;
+        }
+    }
+
+    cout << res << "\n";
 
     return 0;
 }
+
