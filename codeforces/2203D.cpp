c7b13a3b9d1171fe2d47a6dd73041ba2511741be
hungg261 <phanthanhhung.26012010@gmail.com>
2026-05-17 20:56:11 +0700

    Auto-commit [2026-05-17 20:56:11]

--- codeforces/2203D.cpp
+++ codeforces/2203D.cpp
@@ -1,7 +1,7 @@
 /******************************************************************************
-Link: https://codeforces.com/contest/2203/problem/D
+Link: https://codeforces.com/problemset/problem/2203/D
 Code: 2203D
-Time (YYYY-MM-DD-hh.mm.ss): 2026-02-25-22.55.35
+Time (YYYY-MM-DD-hh.mm.ss): 2026-05-17-16.58.12
 *******************************************************************************/
 #include<bits/stdc++.h>
 using namespace std;
@@ -10,64 +10,34 @@ void solve(){
     int n, m;
     cin >> n >> m;
 
-    vector<int> a(n + 1), b(m + 1);
-    for(int i = 1; i <= n; ++i) cin >> a[i];
-    for(int i = 1; i <= m; ++i) cin >> b[i];
-
-    if(find(begin(a) + 1, end(a), 1) != end(a)){
-        cout << "Alice\n";
-        return;
-    }
-
-    map<int, int> mpA, mpB, cntA, cntB;
-    for(int i = 1; i <= n; ++i) mpA[a[i]]++;
-    for(int i = 1; i <= m; ++i) mpB[b[i]]++;
-
     const int MAX = n + m;
-    for(int i = 1; i <= n; ++i){
-        for(int j = a[i]; j <= MAX; j += a[i]){
-            if(!mpB.count(j)) continue;
+    vector<int> freq(MAX + 1, 0);
+    vector<int> dcnt(MAX + 1, 0);
+    vector<int> a(n, 0), b(m, 0);
 
-            ++cntB[j];
+    for(int i = 0; i < n; ++i){
+        cin >> a[i];
+        freq[a[i]]++;
+    }
+    for(int i = 0; i < m; ++i) cin >> b[i];
+
+    for(int i = 1; i <= MAX; ++i){
+        if(freq[i]) for(int j = i; j <= MAX; j += i){
+            dcnt[j] += freq[i];
         }
     }
 
-    multiset<pair<int, int>> ms;
-    for(int i = 1; i <= m; ++i){
-        ms.insert({cntB[b[i]], b[i]});
+    int cnt[2] = {};
+    int middle = 0;
+    for(int j = 0; j < m; ++j){
+        if(dcnt[b[j]] == 0) ++cnt[1];
+        else if(dcnt[b[j]] == n) ++cnt[0];
+        else ++middle;
     }
 
-//    for(auto p: ms){
-//        cerr << p.first << ' ' << p.second << '\n';
-//    }
+    if(middle & 1) ++cnt[0];
 
-    int turn = 0;
-    while(!ms.empty()){
-        if(turn == 0){
-            auto it = prev(ms.end());
-            if(it->first == 0){
-                cout << "Bob\n";
-                return;
-            }
-
-            ms.erase(it);
-        }
-        else{
-            auto it = ms.begin();
-            if(n - it->first == 0){
-                cout << "Alice\n";
-                return;
-            }
-
-            ms.erase(it);
-        }
-
-//        cerr << "-====== p ======-\n";
-//        for(auto p: ms) cerr << p.first << ' ' << p.second << '\n'; cerr << '\n';
-        turn ^= 1;
-    }
-
-    cout << (turn ? "Alice" : "Bob") << '\n';
+    cout << (cnt[1] >= cnt[0] ? "Bob" : "Alice") << "\n";
 }
 
 signed main(){
