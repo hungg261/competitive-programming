5ce324b3dc0691d55465cfcb7dab111ee693df2a
hungg261 <phanthanhhung.26012010@gmail.com>
2026-05-21 10:47:40 +0700

    Auto-commit [2026-05-21 10:47:40]

--- codeforces/1430E.cpp
+++ codeforces/1430E.cpp
@@ -15,7 +15,7 @@ void op(int i){
     ++res;
 }
 
-void solve(){
+void trau(){
     string original = s;
 
     int i = 0, j = n - 1;
@@ -39,6 +39,59 @@ void solve(){
     }
 
     cerr << s << "\n";
+    cout << res << "\n";
+}
+
+struct FenwickTree{
+    int n;
+    vector<int> BIT;
+
+    FenwickTree() = default;
+    FenwickTree(int sz){ n = sz; BIT.resize(sz + 1, 0); }
+
+    void update(int idx, int val){
+        if(idx <= 0) return;
+
+        for(int i = idx; i <= n; i += i & -i){
+            BIT[i] += val;
+        }
+    }
+
+    int get(int idx){
+        if(idx <= 0) return 0;
+
+        int res = 0;
+        for(int i = idx; i > 0; i -= i & -i){
+            res += BIT[i];
+        }
+        return res;
+    }
+
+    int get(int l, int r){ return get(r) - get(l - 1); }
+};
+
+void solve(){
+    set<int> pos[26];
+    for(int i = 0; i < n; ++i)
+        pos[s[i] - 'a'].insert(i + 1);
+
+    vector<int> T(n, -1);
+    for(int i = 0, j = n - 1; i <= j; ++i, --j){
+        int ci = s[i] - 'a', cj = s[j] - 'a';
+        auto itL = pos[cj].begin(), itR = prev(pos[ci].end());
+        T[i] = *itL; T[j] = *itR;
+
+        pos[cj].erase(T[i]);
+        pos[ci].erase(T[j]);
+    }
+
+    FenwickTree fwt(n);
+    long long res = 0;
+    for(int i = n - 1; i >= 0; --i){
+        res += fwt.get(T[i] - 1);
+        fwt.update(T[i], 1);
+    }
+
     cout << res << "\n";
 }
 
