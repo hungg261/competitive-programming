ff6009a7ac124a84c9a7a6d4429d2c915e21de82
hungg261 <phanthanhhung.26012010@gmail.com>
2026-05-28 21:10:01 +0700

    Auto-commit [2026-05-28 21:10:00]

---
+++ yosupo/many_aplusb.cpp
@@ -0,0 +1,27 @@
+/******************************************************************************
+Link: https://judge.yosupo.jp/problem/many_aplusb
+Code: many_aplusb
+Time (YYYY-MM-DD-hh.mm.ss): 2026-05-27-20.52.08
+*******************************************************************************/
+#include<bits/stdc++.h>
+using namespace std;
+
+void solve(){
+    long long a, b;
+    cin >> a >> b;
+
+    cout << (a + b) << "\n";
+}
+
+signed main(){
+    ios_base::sync_with_stdio(0); cin.tie(0);
+
+    int t = 1;
+    cin >> t;
+
+    while(t--){
+        solve();
+    }
+
+    return 0;
+}
