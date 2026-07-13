7a74802397899e30d6227e23af421cc841fc27c7
hungg261 <phanthanhhung.26012010@gmail.com>
2026-07-13 11:58:16 +0700

    Auto-commit [2026-07-13 11:58:16]

--- oj/ojuz/JOI23_currencies.cpp
+++ oj/ojuz/JOI23_currencies.cpp
@@ -1,10 +1,60 @@
 /******************************************************************************
 Link: https://oj.uz/problem/view/JOI23_currencies
 Code: JOI23_currencies
-Time (YYYY-MM-DD-hh.mm.ss): 2026-07-09-11.10.36
+Time (YYYY-MM-DD-hh.mm.ss): 2026-07-13-09.21.40
 *******************************************************************************/
 #include<bits/stdc++.h>
 using namespace std;
+
+#define int long long
+
+const int MAXN = 1e5, MAXM = 1e5, MAXQ = 1e5, MAXLG = __lg(MAXN * 2) + 1;
+int n, m, q;
+struct EdgeTo{ int v; };
+vector<EdgeTo> adj[MAXN + 5];
+
+struct Checkpoint{
+    int C, u;
+    bool operator < (const Checkpoint& other){
+        return C < other.C;
+    }
+} Checkpoints[MAXM + 5];
+struct Query{ int s, t, x, y; } Queries[MAXQ + 5];
+
+int h[MAXN + 5];
+namespace E1{ int tin[MAXN + 5], tout[MAXN + 5], Timer = 0; }
+namespace E2{ int tin[MAXN + 5], Timer = 0; pair<int, int> table[MAXN * 2 + 5][MAXLG + 5]; }
+void dfs(int u = 1, int prv = -1){
+    E1::tin[u] = ++E1::Timer;
+    E2::table[E2::tin[u] = ++E2::Timer][0] = {h[u], u};
+    for(const EdgeTo& e: adj[u]){
+        if(e.v == prv) continue;
+        h[e.v] = h[u] + 1;
+        dfs(e.v, u);
+
+        E2::table[++E2::Timer][0] = {h[u], u};
+    }
+    E1::tout[u] = ++E1::Timer;
+}
+
+void compute(){
+    dfs();
+    for(int j = 1; j <= MAXLG; ++j){
+        for(int i = 1; i + (1 << j) - 1 <= E2::Timer; ++i){
+            E2::table[i][j] = min(E2::table[i][j - 1], E2::table[i + (1 << (j - 1))][j - 1]);
+        }
+    }
+}
+
+int find_lca(int u, int v){
+    int l = E2::tin[u], r = E2::tin[v];
+    if(l > r) swap(l, r);
+
+    int bit = __lg(r - l + 1);
+    return min(E2::table[l][bit], E2::table[r - (1 << bit) + 1][bit]).second;
+}
+
+namespace Solve{
 
 struct FenwickTree{
     int n;
@@ -34,89 +84,109 @@ struct FenwickTree{
     int get(int l, int r){ return get(r) - get(l - 1); }
 };
 
-const int MAXN = 1e5, MAXM = 1e5, MAXQ = 1e5, MAXLG = __lg(MAXN) + 1;
-int n, m, q;
-vector<int> adj[MAXN + 5];
-int table[MAXN + 5][MAXLG + 5], h[MAXN + 5];
+FenwickTree fwt;
 
-vector<pair<int, int>> edges;
-
-int w[MAXN + 5], g[MAXN + 5];
-
-void dfs1(int u = 1, int prv = -1){
-    for(int j = 1; j <= MAXLG; ++j)
-        table[u][j] = table[table[u][j - 1]][j - 1];
-
-    for(int v: adj[u]){
-        if(v == prv) continue;
-
-        table[v][0] = u;
-        h[v] = h[u] + 1;
-        dfs1(v, u);
-    }
+inline void add(int u, int w){
+    fwt.update(E1::tin[u], w);
+    fwt.update(E1::tout[u], -w);
 }
+inline int get(int u){ return fwt.get(E1::tin[u]); }
+inline int get(int u, int v){ return get(u) + get(v) - get(find_lca(u, v)) * 2; }
 
-void dfs2(int u = 1, int prv = -1){
-    for(int v: adj[u]){
-        if(v == prv) continue;
+int L[MAXQ + 5], R[MAXQ + 5];
+vector<int> candidates[MAXM + 5];
+void solve(){
+    for(int i = 1; i <= q; ++i)
+        L[i] = 0, R[i] = m;
 
-        g[v] = g[u] + w[v];
-        dfs2(v, u);
-    }
-}
+    sort(Checkpoints + 1, Checkpoints + m + 1);
 
-int find_lca(int u, int v){
-    if(h[u] > h[v]) swap(u, v);
+    while(true){
+        bool changed = false;
+        for(int i = 1; i <= q; ++i){
+            if(L[i] >= R[i]) continue;
 
-    if(h[u] != h[v]){
-        int diff = h[v] - h[u];
-        for(int b = MAXLG; b >= 0; --b)
-            if(diff >> b & 1) v = table[v][b];
-    }
+            int mid = (L[i] + R[i] + 1) >> 1;
+            candidates[mid].push_back(i);
+            changed = true;
+        }
+        if(!changed) break;
 
-    if(u == v) return u;
+        fwt = FenwickTree(E1::Timer);
+        for(int i = 1; i <= m; ++i){
+            const Checkpoint& Cp = Checkpoints[i];
+            add(Cp.u, Cp.C);
 
-    for(int b = MAXLG; b >= 0; --b){
-        if(table[u][b] != table[v][b]){
-            u = table[u][b];
-            v = table[v][b];
+            for(int x: candidates[i]){
+                const Query& Q = Queries[x];
+                if(get(Q.s, Q.t) <= Q.y) L[x] = i;
+                else R[x] = i - 1;
+            }
+
+            candidates[i].clear();
         }
     }
 
-    return table[u][0];
+    fwt = FenwickTree(E1::Timer);
+
+    vector<int> qs(q); iota(begin(qs), end(qs), 1);
+    sort(begin(qs), end(qs), [&](int x, int y){ return L[x] < L[y]; });
+    int pos = 1;
+    for(int i = 0; i < q; ++i){
+        while(pos <= L[qs[i]]) add(Checkpoints[pos++].u, 1);
+
+        const Query& Q = Queries[qs[i]];
+        L[qs[i]] = get(Q.s, Q.t);
+    }
+    while(pos <= m) add(Checkpoints[pos++].u, 1);
+
+    for(int i = 1; i <= q; ++i){
+        const Query& Q = Queries[i];
+        int gold = get(Q.s, Q.t) - L[i];
+
+        if(gold > Q.x) cout << "-1\n";
+        else cout << Q.x - gold << "\n";
+    }
 }
 
-inline int calc(int u, int v){ return g[u] + g[v] - g[find_lca(u, v)] * 2; }
+}
 
 signed main(){
     ios_base::sync_with_stdio(0); cin.tie(0);
 
     cin >> n >> m >> q;
+
+    vector<pair<int, int>> edges;
+    edges.emplace_back();
     for(int i = 1; i < n; ++i){
         int a, b;
         cin >> a >> b;
 
-        adj[a].push_back(b);
-        adj[b].push_back(a);
+        adj[a].push_back({b});
+        adj[b].push_back({a});
         edges.emplace_back(a, b);
     }
 
-    dfs1();
+    compute();
 
     for(int i = 1; i <= m; ++i){
         int P, C;
         cin >> P >> C;
 
-        --P;
         int u, v; tie(u, v) = edges[P];
-        if(table[v][0] != u) swap(u, v);
+        if(E1::tin[u] > E1::tin[v]) swap(u, v);
 
-        w[v] = C;
+        Checkpoints[i] = {C, v};
     }
 
-    dfs2();
+    for(int i = 1; i <= q; ++i){
+        int s, t, x, y;
+        cin >> s >> t >> x >> y;
 
+        Queries[i] = {s, t, x, y};
+    }
 
+    Solve::solve();
 
     return 0;
 }
